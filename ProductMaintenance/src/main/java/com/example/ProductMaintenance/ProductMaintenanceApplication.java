0124cf32fb33e95dffd3b6e7abbbc02010fe7189
Trong Lan <91108170+TrongLan@users.noreply.github.com>
2022-05-08 12:49:17 +0700

    Project ban đầu

---
+++ ProductMaintenance/src/main/java/com/example/ProductMaintenance/ProductMaintenanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProductMaintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductMaintenanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductMaintenanceApplication.class, args);
+	}
+
+}
---
+++ ProductMaintenance/src/test/java/com/example/ProductMaintenance/ProductMaintenanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProductMaintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductMaintenanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
