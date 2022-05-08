e418a14334010678321a97e7d551288cadc75e4b
Trong Lan <91108170+TrongLan@users.noreply.github.com>
2022-05-08 14:47:26 +0700

    Code lớp controller

--- ProductMaintenance/src/main/java/com/example/ProductMaintenance/Controller/ProductController.java
+++ ProductMaintenance/src/main/java/com/example/ProductMaintenance/Controller/ProductController.java
@@ -6,11 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.ProductMaintenance.Entity.Product;
 import com.example.ProductMaintenance.Repository.ProductRepository;
 
 @Controller
+@RequestMapping("/ProductMaintenance")
 public class ProductController {
 	@Autowired
 	private ProductRepository productRepo;
@@ -21,4 +25,40 @@ public class ProductController {
 		mod.addAttribute("dssp", product_list);
 		return "viewProducts";
 	}
+	
+	@GetMapping("/deleteProduct")
+	public String openConfirmDelete(Model mod, @RequestParam(name = "pcode",required = true) String pcode) {
+		Product p = productRepo.findProductByCode(pcode);
+		mod.addAttribute("product", p);
+		return "confirmDelete";
+	}
+	
+	@GetMapping("/comfirmDelete")
+	public String afterDelete(Model mod, @RequestParam(name = "pcode",required = true) String pcode, @RequestParam(name = "confirm",required = false) String agree) {
+		if(agree.equals("yes")) {
+			productRepo.delete(productRepo.findProductByCode(pcode));	
+		}
+		return "redirect:/displayAllProducts";
+	}
+	
+	@GetMapping("/updateProduct")
+	public String openUpdatePage(Model mod, @RequestParam(name = "pcode",required = true) String pcode) {
+		Product p = productRepo.findProductByCode(pcode);
+		mod.addAttribute("product", p);
+		return "updateProduct";
+	}
+	
+	@GetMapping("/addNewProduct")
+	public String addNewProduct(Model mod) {
+		Product p = new Product();
+		mod.addAttribute("product", p);
+		return "updateProduct";
+	}
+	
+	@PostMapping("/updateProduct")
+	public String afterUpdateProduct(Product p) {
+		productRepo.save(p);
+		return "";
+	}
+	
 }
--- ProductMaintenance/src/main/java/com/example/ProductMaintenance/Repository/ProductRepository.java
+++ ProductMaintenance/src/main/java/com/example/ProductMaintenance/Repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.example.ProductMaintenance.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.ProductMaintenance.Entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long>{
-
+	@Query(value = "SELECT * FROM Product WHERE code = :pcode", nativeQuery = true)
+	Product findProductByCode(String code);
 }
